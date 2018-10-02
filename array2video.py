da4400c91b03ea4231ab4c6735d688d354b2ac96
André Cavalcante <abcborges@gmail.com>
2018-10-02 16:31:34 -0300

    Update array2video.py

--- array2video.py
+++ array2video.py
@@ -1,15 +1,5 @@
-"""
-===========
-MovieWriter
-===========
 
-This example uses a MovieWriter directly to grab individual frames and write
-them to a file. This avoids any event loop integration, but has the advantage
-of working with even the Agg backend. This is not recommended for use in an
-interactive setting.
-
-"""
-# -*- noplot -*-
+# does not require libx264 
 
 import numpy as np
 import matplotlib
@@ -33,3 +23,5 @@ with writer.saving(fig, "writer_test.mp4", dpi=60):
         
         plt.imshow(frame)
         writer.grab_frame()
+        
+        plt.clf() # make sure to clear plot after each frame is grabbed so it does not slow down
